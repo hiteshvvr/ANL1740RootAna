407cc5700fffee4aa7a830786986ea811b584777
hiteshvvr <hitesh.vvr@gmail.com>
2019-02-01 01:30:17 +0200

    modified slightly

--- TV1740RawData.cxx
+++ TV1740RawData.cxx
@@ -1,78 +1,37 @@
-#include "TV1720RawData.h"
+#include "TV1740RawData.h"
 
 #include <iomanip>
 #include <iostream>
 
-void TV1720RawData::HandlZLECompressedData(){
-
-  // >>> Loop over ZLE data and fill up  
-
-  uint32_t chMask    = GetData32()[1] & 0xFF;
-  uint32_t iPtr=4;
-  for(int iCh=0; iCh<8; iCh++){
-    if (chMask & (1<<iCh)){
-      uint32_t chSize = GetData32()[iPtr];
-      uint32_t iChPtr = 1;// The chSize space is included in chSize
-      uint32_t iBin=0;
-      iPtr++;
-
-      TV1720RawChannel channel(iCh, IsZLECompressed());
-      
-    while(iChPtr<chSize){
-	uint32_t goodData = ((GetData32()[iPtr]>>31) & 0x1);
-	uint32_t nWords = (GetData32()[iPtr] & 0xFFFFF);  
-	if(goodData){ 
-	  std::vector<uint32_t> samples;
-	  for(uint32_t iWord=0; iWord<nWords; iWord++){
-	    iPtr++;
-	    iChPtr++;	   
-	    samples.push_back((GetData32()[iPtr]&0xFFF));
-	    samples.push_back(((GetData32()[iPtr]>>16)&0xFFF));
-	  }
-	  channel.AddZlePulse(TV1720RawZlePulse(iBin, samples));
-	}
-
-	iBin += (nWords*2);
-	iChPtr++;
-	iPtr++;	
-
-      }
-
-      fMeasurements.push_back(channel);
-    }
-  }
-
-}
-
-void TV1720RawData::HandlUncompressedData(){
+void TV1740RawData::HandlUncompressedData(){
 
   // Skip the header.
   uint32_t iPtr=4;
-  uint32_t chMask    = GetData32()[1] & 0xFF;
+  uint32_t chgrMask    = GetData32()[1] & 0xFF;
   // const  uint32_t *fData = GetData32();
   // printf("%ld %ld %ld %ld \n",fData[0],fData[1],fData[2],fData[3]);
 
-  int nActiveChannels=0;
+  int nActiveGroup=0;
   for(int iCh=0; iCh<8; iCh++){
-    if(chMask & (1<<iCh))
-      nActiveChannels++;
+    if(chgrMask & (1<<iCh))
+      nActiveGroup++;
   }
-  // Assume that we have readout the same number of samples for each channel.
+  // Assume that we have readout the same number of samples for each Group and each Channel.
   // The number of 32 bit double-samples per channel is then
-  // N32samples = (bank size - 4)/ nActiveChannels 
-  int N32samples = (GetEventSize() - 4)/ nActiveChannels;
+  // N32samples = (bank size - 4)/ ( 8 * nActiveGroup)
+  int N32samples = (GetEventSize() - 4)/ (8*nActiveGroup);
 
   // Loop over channels
   for(int iCh=0; iCh<8; iCh++){
         
     if(!(chMask & (1<<iCh))){
       // Add empty channel data objects, to keep vector simple.
-      TV1720RawChannel channel(iCh, IsZLECompressed());
+      TV1740RawChannel channel(iCh, IsZLECompressed());
       fMeasurements.push_back(channel);
       continue;
     }
 
-    TV1720RawChannel channel(iCh, IsZLECompressed());
+    TV1740RawChannel channel(iCh, IsZLECompressed());
       
     for(int j = 0; j < N32samples; j++){
       uint32_t samp1 = (GetData32()[iPtr]&0xFFF);
@@ -89,7 +48,7 @@ void TV1720RawData::HandlUncompressedData(){
 }
 
 
-TV1720RawData::TV1720RawData(int bklen, int bktype, const char* name, void *pdata):
+TV1740RawData::TV1740RawData(int bklen, int bktype, const char* name, void *pdata):
     TGenericData(bklen, bktype, name, pdata)
 {
   
@@ -108,9 +67,9 @@ TV1720RawData::TV1720RawData(int bklen, int bktype, const char* name, void *pdat
 
 }
 
-void TV1720RawData::Print(){
+void TV1740RawData::Print(){
 
-  std::cout << "V1720 decoder for bank " << GetName().c_str() << std::endl;
+  std::cout << "V1740 decoder for bank " << GetName().c_str() << std::endl;
   std::cout << "Bank size: " << GetEventSize() << std::endl;
   
   if( IsZLECompressed())
@@ -127,7 +86,7 @@ void TV1720RawData::Print(){
   std::cout << "Number of channels with data: " << GetNChannels() << std::endl;
   for(int i = 0 ; i < GetNChannels(); i++){
 
-    TV1720RawChannel channelData = GetChannelData(i);
+    TV1740RawChannel channelData = GetChannelData(i);
 
     std::cout << "Channel: " << channelData.GetChannelNumber() << std::endl;
     
@@ -135,7 +94,7 @@ void TV1720RawData::Print(){
       std::cout << "Number of ZLE pulses: " <<  channelData.GetNZlePulses() << std::endl;
       for(int j = 0; j < channelData.GetNZlePulses(); j++){
 	std::cout << "Pulse: " << j << std::endl;
-	TV1720RawZlePulse pulse = channelData.GetZlePulse(j);	
+	TV1740RawZlePulse pulse = channelData.GetZlePulse(j);	
 	std::cout << "first sample bin: " << pulse.GetFirstBin() << std::endl;
 	std::cout << "Samples ("<< pulse.GetNSamples()<<  " total): " <<std::endl;
 	for(int k = 0; k < pulse.GetNSamples(); k++){
