9e77386ccfa197b75801d2fdfdc5b2466413c8b5
hiteshvvr <hitesh.vvr@gmail.com>
2019-02-03 21:39:50 +0200

    Modified midas2root and Makefile

--- TV1740RawData.cxx
+++ TV1740RawData.cxx
@@ -12,27 +12,13 @@ void TV1740RawData::HandlUncompressedData(){
   // printf("%ld %ld %ld %ld \n",fData[0],fData[1],fData[2],fData[3]);
 
   int nActiveGroup=0;
-<<<<<<< HEAD
   for(int iGr=0; iGr<8; iGr++){
     if(chgrMask & (1<<iGr))
-=======
-<<<<<<< HEAD
-  for(int iGr=0; iGr<8; iGr++){
-    if(chgrMask & (1<<iGr))
-=======
-  for(int iCh=0; iCh<8; iCh++){
-    if(chgrMask & (1<<iCh))
->>>>>>> 407cc5700fffee4aa7a830786986ea811b584777
->>>>>>> dc7d55c95ec340b7d1e102a625c4cd228ddd79c4
       nActiveGroup++;
   }
   // Assume that we have readout the same number of samples for each Group and each Channel.
   // The number of 32 bit double-samples per channel is then
   // N32samples = (bank size - 4)/ ( 8 * nActiveGroup)
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> dc7d55c95ec340b7d1e102a625c4cd228ddd79c4
   int N32samples = (GetEventSize() - 4)/ (8 * nActiveGroup);
 
   // Loop over channels
@@ -74,37 +60,6 @@ void TV1740RawData::HandlUncompressedData(){
       }
       fMeasurements.push_back(channel);
     }  
-<<<<<<< HEAD
-=======
-=======
-  int N32samples = (GetEventSize() - 4)/ (8*nActiveGroup);
-
-  // Loop over channels
-  for(int iCh=0; iCh<8; iCh++){
-        
-    if(!(chMask & (1<<iCh))){
-      // Add empty channel data objects, to keep vector simple.
-      TV1740RawChannel channel(iCh, IsZLECompressed());
-      fMeasurements.push_back(channel);
-      continue;
-    }
-
-    TV1740RawChannel channel(iCh, IsZLECompressed());
-      
-    for(int j = 0; j < N32samples; j++){
-      uint32_t samp1 = (GetData32()[iPtr]&0xFFF);
-      uint32_t samp2 = ((GetData32()[iPtr]>>16)&0xFFF);
-      
-      channel.AddADCSample(samp1);
-      channel.AddADCSample(samp2);      
-      iPtr++;
-    }
-
-    fMeasurements.push_back(channel);
-
-  }  
->>>>>>> 407cc5700fffee4aa7a830786986ea811b584777
->>>>>>> dc7d55c95ec340b7d1e102a625c4cd228ddd79c4
 }
 
 
@@ -119,19 +74,7 @@ TV1740RawData::TV1740RawData(int bklen, int bktype, const char* name, void *pdat
   
 
 
-<<<<<<< HEAD
     HandlUncompressedData();
-=======
-<<<<<<< HEAD
-    HandlUncompressedData();
-=======
-  if(IsZLECompressed()){
-    HandlZLECompressedData();
-  }else{
-    HandlUncompressedData();
-  }
->>>>>>> 407cc5700fffee4aa7a830786986ea811b584777
->>>>>>> dc7d55c95ec340b7d1e102a625c4cd228ddd79c4
 
 }
 
@@ -140,17 +83,6 @@ void TV1740RawData::Print(){
   std::cout << "V1740 decoder for bank " << GetName().c_str() << std::endl;
   std::cout << "Bank size: " << GetEventSize() << std::endl;
   
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-  if( IsZLECompressed())
-    std::cout << "Data is ZLE compressed." << std::endl;
-  else
-    std::cout << "Data is not ZLE compressed." << std::endl;
-
->>>>>>> 407cc5700fffee4aa7a830786986ea811b584777
->>>>>>> dc7d55c95ec340b7d1e102a625c4cd228ddd79c4
   std::cout << "Channel Mask : " << GetChannelMask() << std::endl;
    
   std::cout << "Event counter : " << GetEventCounter() << std::endl;
@@ -164,28 +96,6 @@ void TV1740RawData::Print(){
 
     std::cout << "Channel: " << channelData.GetChannelNumber() << std::endl;
     
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-    if(IsZLECompressed()){
-      std::cout << "Number of ZLE pulses: " <<  channelData.GetNZlePulses() << std::endl;
-      for(int j = 0; j < channelData.GetNZlePulses(); j++){
-	std::cout << "Pulse: " << j << std::endl;
-	TV1740RawZlePulse pulse = channelData.GetZlePulse(j);	
-	std::cout << "first sample bin: " << pulse.GetFirstBin() << std::endl;
-	std::cout << "Samples ("<< pulse.GetNSamples()<<  " total): " <<std::endl;
-	for(int k = 0; k < pulse.GetNSamples(); k++){
-	  std::cout << pulse.GetSample(k) << ", ";
-	  if(k%12 == 11) std::cout << std::endl;
-	}
-	std::cout << std::endl;
-	
-      }
-    }
-
->>>>>>> 407cc5700fffee4aa7a830786986ea811b584777
->>>>>>> dc7d55c95ec340b7d1e102a625c4cd228ddd79c4
   }
 
 }
